32d3a6501225c3234db591bb0418f7f999630c38
Lilly <143411593+ngosterly@users.noreply.github.com>
2024-11-01 07:32:20 +0000

    Updated files with new content

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// fade in for the introductory page
 window.onload = function() {
     const words = document.querySelectorAll('.fade-in');
     words.forEach((word, index) => {
@@ -7,3 +8,30 @@ window.onload = function() {
         }, index * 750);
     });
 };
+
+// fade in for about me and contact me 
+function fadeInSection() {
+    const sections = document.querySelectorAll('.fade-in-section');
+    const options = {
+        root: null,
+        threshold: 0.2, 
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('is-visible');
+            } else {
+                entry.target.classList.remove('is-visible');
+            }
+        });
+    }, options);
+
+    sections.forEach((section, index) => {
+   
+        section.style.transitionDelay = `${index * 250}ms`; 
+        observer.observe(section);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', fadeInSection);
